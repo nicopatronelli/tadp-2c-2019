d8f4606fa55b7651e8b928aec700086c6a4c3451
nicopatronelli <nicopatronelli@gmail.com>
2019-10-24 14:53:02 -0300

    Cambio attr_persistibles de lista a hash

--- ruby/spec/aux_test_spec.rb
+++
@@ -1,7 +0,0 @@
-describe "Persistible.exists_id?" do
-  it "exists_id? :id" do
-    pikachu = Pokemon.builder(25, "Raichu", "Eléctrico", false)
-    pikachu.save!
-    expect(Pokemon.exists_id? pikachu.id).to eq true
-  end
-end
